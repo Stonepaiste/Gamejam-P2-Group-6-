66f081149b30798d1da17d5f7d3d581300c7dfc9
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2024-04-23 19:43:56 +0200

    Map elements as sections

--- Assets/Scripts/CelestialBodySpawner.cs
+++ Assets/Scripts/CelestialBodySpawner.cs
@@ -1,26 +1,19 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class CelestialBodySpawner : MonoBehaviour
 {
     [SerializeField]
-    private GameObject meteoritePrefab;
-    [SerializeField]
-    private GameObject cheesePrefab; 
-    [SerializeField]
-    private GameObject stinkyCheesePrefab;
+    private GameObject meteoritePrefab, cheesePrefab, stinkyCheesePrefab;
     
     [SerializeField]
-    private int cheeseAmount = 100;
+    private static int defaultCheeseAmount = 10;
     [SerializeField]
-    private int meteoriteAmount = 500;
+    private static int defaultMeteoriteAmount = 7;
     [SerializeField]
-    private int stinkyCheeseOccurrence= 10;
+    private static int defaultStinkyCheeseInterval= 9;
     
     [SerializeField]
     private static float waveOffsetMax = 4f;
@@ -32,197 +25,208 @@ public class CelestialBodySpawner : MonoBehaviour
     [SerializeField]
     private float meteoriteOffsetMin = -3f;
     
-    private List<float> phaseShifts = new List<float>();
+    private List<float> wavePhases = new List<float>();
     private List<float> waveOffsets = new List<float>();
     private List<Vector3> cheesePositions = new List<Vector3>();
     
     
-    [System.Serializable]
+    [Serializable]
     public class CelestialBodyRange
     {
-        [Range(0, 100)]
-        public float min = 0;
-        [Range(0, 100)]
-        public float max = 100;
+        [Range(0, 10)]
+        public int min = 0;
+        [Range(0, 10)]
+        public int max = 10;
     }
     
     
-    [System.Serializable]
+    [Serializable]
     public class MapSection
     {
         public float wavePhase;
         public float waveOffset;
         public float waveAmplitude;
-        public int cheeseAmount = 100;
-        public int meteoriteAmount = 500;
-        public int stinkyCheeseOccurrence = 10;
-        public CelestialBodyRange meteoriteRange = new CelestialBodyRange
+        public int cheeseAmount = 10;
+        public int meteoriteAmount = 10;
+        public int stinkyCheeseOccurrence = 1;
+        public CelestialBodyRange meteoriteRange = new();
+        public CelestialBodyRange cheeseRange = new();
+
+        public void Initialize()
         {
-            min = 0,
-            max = 100
-        };
-        public CelestialBodyRange cheeseRange = new CelestialBodyRange();
+            if (wavePhase == 0.0f)
+            {
+                wavePhase = Random.Range(-20f, 20f);
+            }
+            if (waveOffset == 0.0f)
+            {
+                waveOffset = Random.Range(waveOffsetMin, waveOffsetMax);
+            }
+            if (waveAmplitude == 0.0f)
+            {
+                waveAmplitude = Random.Range(.5f, 2f);
+            }
+            if (cheeseAmount == 0)
+            {
+                cheeseAmount = defaultCheeseAmount;
+            }
+            if (meteoriteAmount == 0)
+            {
+                meteoriteAmount = defaultMeteoriteAmount;
+            }
+            if (stinkyCheeseOccurrence == 0)
+            {
+                stinkyCheeseOccurrence = defaultStinkyCheeseInterval;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Wave Phase: {wavePhase}, Wave Offset: {waveOffset}, Wave Amplitude: {waveAmplitude}, Cheese Amount: {cheeseAmount}, Meteorite Amount: {meteoriteAmount}, Stinky Cheese Occurrence: {stinkyCheeseOccurrence}, Meteorite Range: {meteoriteRange.min} - {meteoriteRange.max}, Cheese Range: {cheeseRange.min} - {cheeseRange.max}";
+        }
     }
+
     
     [SerializeField]
-    List<MapSection> mapSections = new List<MapSection>();
+    List<MapSection> mapSections = new();
     
-    
-    private TextMesh textMesh;
 
     void Start()
     {
-        int phaseCounter = 0;
-        float phaseShift = Random.Range(-20f, 20f);
-        float waveOffset = Random.Range(waveOffsetMin, waveOffsetMax);
-        int phaseShiftInteval = Random.Range(5, 10);
-        
-        int stinkyCheeseCounter = 0;
-        int stinkyCheeseInterval = Random.Range((int)(stinkyCheeseOccurrence * .5), stinkyCheeseOccurrence * 2);
-        
-        
-        int numberOfCheeses = (int)Mathf.Round(Random.Range((float)(cheeseAmount * .5), cheeseAmount * 2)); // Number of cheeses to spawn
-        // int numberOfMeteorites = (int)Mathf.Round(Random.Range((float)(meteoriteAmount * .5), meteoriteAmount * 2));; // Number of meteorites to spawn
-        int numberOfMeteorites = meteoriteAmount;
-        float cheeseDensity = numberOfCheeses / 100f; // Cheeses per x unit
-        float meteoriteDensity = numberOfMeteorites / 100f; // Meteorites per x unit
-        
-        
-        
-        // Spawn cheeses alone the x-axis following a sine wave
-        for (float x = 0; x < numberOfCheeses / cheeseDensity; x += 1/cheeseDensity)
+        float currentX = 0;
+        foreach (MapSection mapSection in mapSections)
         {
-            // float x = i * 1;
-            if (phaseCounter >= phaseShiftInteval)
+            mapSection.Initialize();
+            Debug.Log(mapSection.ToString());   
+            
+            int phaseCounter = 0, stinkyCheeseCounter = 0;
+            float wavePhase = mapSection.wavePhase;
+            float waveOffset = mapSection.waveOffset;
+            int phaseShiftInteval = Random.Range(5, 10);
+            
+            int stinkyCheeseInterval = Random.Range((int)(mapSection.stinkyCheeseOccurrence * .5), mapSection.stinkyCheeseOccurrence * 2);
+            
+            int numberOfCheeses = (int)Mathf.Round(Random.Range((float)(mapSection.cheeseAmount * .5), mapSection.cheeseAmount * 2)); // Number of cheeses to spawn
+            int numberOfMeteorites = mapSection.meteoriteAmount;
+            float cheeseDensity = numberOfCheeses / 10f; // Cheeses per x unit
+            float meteoriteDensity = numberOfMeteorites / 10f; // Meteorites per x unit
+            
+            
+            // TODO: Extract the sine wave generation to the upper loop to have the same data available for both cheese and meteorite generation.
+            //       Currently, the meteorites are generated based on the cheeses, which is not ideal.
+            // BUG: Meteorites are not created in sections without cheeses.
+            
+            
+            // Spawn cheeses alone the x-axis following a sine wave
+            for (float x = mapSection.cheeseRange.min + currentX; x < mapSection.cheeseRange.max + currentX; x += 1/cheeseDensity)
             {
-                phaseShift = Random.Range(-50f, 50f);
-                phaseCounter = 0;
-                phaseShiftInteval = Random.Range(5, 10);
-                waveOffset = Random.Range(waveOffsetMin, waveOffsetMax);
-                // Debug.Log("Phase shift: " + phaseShift);
-            }
-            
-            float y = Mathf.Sin(x + phaseShift) + waveOffset;
-            // float y = Mathf.Sin(x);
-            // Debug.Log("PhaseShift: " + phaseShift + " x: " + x + " y: " + y);
-            Vector3 position = new Vector3(x, y, 0);
-            Quaternion rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
-
-            GameObject cheese;
-            if (stinkyCheeseCounter >= stinkyCheeseInterval)
-            {
-                cheese = Instantiate(stinkyCheesePrefab, transform.position + position, rotation);
-                stinkyCheeseCounter = 0;
-                stinkyCheeseInterval = Random.Range((int)(stinkyCheeseOccurrence * .5), stinkyCheeseOccurrence * 2);
-                // Debug.Log("Stinky cheese spawned at " + position);
-            }
-            else
-            {
-                cheese = Instantiate(cheesePrefab, transform.position + position, rotation);
-            }
-            cheese.transform.parent = this.transform;
-            
-            phaseShifts.Add(phaseShift);
-            waveOffsets.Add(waveOffset);
-            cheesePositions.Add(position);
-            
-            phaseCounter++;
-            stinkyCheeseCounter++;
-        }
-        
-        // Spawn meteorites around the cheeses randomly on the y-axis
-        for (float x = 0; x < numberOfMeteorites / meteoriteDensity; x += 1/meteoriteDensity)
-        {
-            // Debug.Log("Meteorite spawn" + " _phaseShift: " + phaseShift + " _waveOffset: " + waveOffset);
-            Quaternion rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
-            // float y = Mathf.Sin(x + _phaseShift) + _waveOffset;
-            // float y = Mathf.Sin(x);
-            float meteoriteOffset = Random.Range(meteoriteOffsetMin, meteoriteOffsetMax);
-            // float meteoriteOffset = 0f;
-            
-            // DEBUG OBJECTs
-            /*GameObject oriMeteorite = Instantiate(meteoritePrefab, transform.position + new Vector3(x, y, 1), rotation);
-            oriMeteorite.transform.parent = this.transform;
-            SpriteRenderer spriteRenderer = oriMeteorite.transform.GetChild(0).GetComponent<SpriteRenderer>();
-            spriteRenderer.color = new Color(1, 0, 0, 1);
-            
-            
-            float distanceFromCheese = Mathf.Abs(meteoriteOffset -y);
-            // Add a text object to the Gameobject that shows the offset
-            GameObject textObject = new GameObject("Text");
-            textObject.transform.parent = oriMeteorite.transform;
-            TextMesh textMesh = textObject.AddComponent<TextMesh>();
-            textMesh.text = Math.Round(meteoriteOffset, 2).ToString() + "\n" + distanceFromCheese.ToString() + "\n" + (meteoriteOffset - y).ToString();
-            textMesh.fontSize = 25;
-            textMesh.anchor = TextAnchor.MiddleCenter;
-            textMesh.characterSize = 0.1f;
-            textMesh.offsetZ = -10f;
-            textMesh.color = Color.white;
-            textObject.transform.position = oriMeteorite.transform.position + new Vector3(0, 0, 0);*/
-            
-            List<float> cheesePositionsX = cheesePositions.ConvertAll(vector => vector.x);
-            
-            int index = cheesePositionsX.BinarySearch(x);
-            if (index < 0)
-            {
-                index = ~index;
-            }
-            int lowerIndex = Math.Max(0, index -1);
-            int upperIndex = Math.Min(cheesePositions.Count - 1, index + 1);
-            
-            float t = (x - cheesePositions[lowerIndex].x) / (cheesePositions[upperIndex].x - cheesePositions[lowerIndex].x);
-            phaseShift = Mathf.Lerp(phaseShifts[lowerIndex], phaseShifts[upperIndex], t);
-            waveOffset = Mathf.Lerp(waveOffsets[lowerIndex], waveOffsets[upperIndex], t);
-            float y = Mathf.Sin(x + phaseShift) + waveOffset;
-            
-            Vector3 position;
-            
-            if (Math.Abs(meteoriteOffset - y) < 1f)
-            {
-                if (meteoriteOffset -y < 0)
+                if (phaseCounter >= phaseShiftInteval)
                 {
-                    position = new Vector3(x, meteoriteOffset - 1, 0);
+                    wavePhase = Random.Range(-50f, 50f);
+                    phaseCounter = 0;
+                    phaseShiftInteval = Random.Range(5, 10);
+                    waveOffset = Random.Range(waveOffsetMin, waveOffsetMax);
+
+                }
+                
+                float y = Mathf.Sin(x + wavePhase) + waveOffset;
+                
+                Vector3 position = new Vector3(x, y, 0);
+                Quaternion rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+
+                GameObject cheese;
+                if (stinkyCheeseCounter >= stinkyCheeseInterval)
+                {
+                    cheese = Instantiate(stinkyCheesePrefab, transform.position + position, rotation);
+                    stinkyCheeseCounter = 0;
+                    stinkyCheeseInterval = Random.Range((int)(mapSection.stinkyCheeseOccurrence * .5), mapSection.stinkyCheeseOccurrence * 2);
                 }
                 else
                 {
-                    position = new Vector3(x, meteoriteOffset + 1, 0);
+                    cheese = Instantiate(cheesePrefab, transform.position + position, rotation);
                 }
-            }
-            else
-            {
-                position = new Vector3(x, meteoriteOffset, 0);
+                cheese.transform.parent = transform;
+                
+                wavePhases.Add(wavePhase);
+                waveOffsets.Add(waveOffset);
+                cheesePositions.Add(position);
+                
+                phaseCounter++;
+                stinkyCheeseCounter++;
             }
             
-            List<float> distances = new List<float>();
-
-            foreach (Vector3 cheesePosition in cheesePositions)
+            // Spawn meteorites around the cheeses randomly on the y-axis
+            for (float x = mapSection.meteoriteRange.min + currentX; x < mapSection.meteoriteRange.max + currentX; x += 1/meteoriteDensity)
             {
-                if (Math.Abs(cheesePosition.x - x) <= 1)
+                Quaternion rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+                float meteoriteOffset = Random.Range(meteoriteOffsetMin, meteoriteOffsetMax);
+                
+                List<float> cheesePositionsX = cheesePositions.ConvertAll(vector => vector.x);
+                int index = cheesePositionsX.BinarySearch(x);
+                if (index < 0)
                 {
-                    float distance = Vector3.Distance(cheesePosition, position);
-                    distances.Add(distance);
+                    index = ~index;
                 }
-            }
-            
-            GameObject meteorite = Instantiate(meteoritePrefab, transform.position + position, rotation);
-            meteorite.transform.parent = this.transform;
-            
-            string distancesStr = "";
+                int lowerIndex = Math.Max(0, index -1);
+                int upperIndex = Math.Min(cheesePositions.Count - 1, index + 1);
+                
+                float t = (x - cheesePositions[lowerIndex].x) / (cheesePositions[upperIndex].x - cheesePositions[lowerIndex].x);
+                wavePhase = Mathf.Lerp(wavePhases[lowerIndex], wavePhases[upperIndex], t);
+                waveOffset = Mathf.Lerp(waveOffsets[lowerIndex], waveOffsets[upperIndex], t);
+                float y = Mathf.Sin(x + wavePhase) + waveOffset;
+                
+                Vector3 position;
+                
+                if (Math.Abs(meteoriteOffset - y) < 1f)
+                {
+                    if (meteoriteOffset -y < 0)
+                    {
+                        position = new Vector3(x, meteoriteOffset - 1, 0);
+                    }
+                    else
+                    {
+                        position = new Vector3(x, meteoriteOffset + 1, 0);
+                    }
+                }
+                else
+                {
+                    position = new Vector3(x, meteoriteOffset, 0);
+                }
+                
+                GameObject meteorite = Instantiate(meteoritePrefab, transform.position + position, rotation);
+                meteorite.transform.parent = transform;
+                
+                
+                // DEBUG - Display distances between meteorites and cheeses
+                List<float> distances = new List<float>();
 
-            foreach (float distance in distances)
-            {
-                distancesStr += distance.ToString() + "\n";
+                foreach (Vector3 cheesePosition in cheesePositions)
+                {
+                    if (Math.Abs(cheesePosition.x - x) <= 1)
+                    {
+                        float distance = Vector3.Distance(cheesePosition, position);
+                        distances.Add(distance);
+                    }
+                }
+                
+                string distancesStr = "";
+
+                foreach (float distance in distances)
+                {
+                    distancesStr += Math.Round(distance, 2).ToString() + "\n";
+                }
+                /*GameObject textObject = new GameObject("Text");
+                textObject.transform.parent = meteorite.transform;
+                TextMesh textMesh = textObject.AddComponent<TextMesh>();
+                textMesh.text = distancesStr;
+                textMesh.fontSize = 25;
+                textMesh.anchor = TextAnchor.MiddleCenter;
+                textMesh.characterSize = 0.1f;
+                textMesh.offsetZ = -1f;
+                textMesh.color = Color.white;
+                textObject.transform.position = meteorite.transform.position + new Vector3(0, 0, 0);*/
             }
-            GameObject textObject = new GameObject("Text");
-            textObject.transform.parent = meteorite.transform;
-            TextMesh textMesh = textObject.AddComponent<TextMesh>();
-            textMesh.text = distancesStr;
-            textMesh.fontSize = 25;
-            textMesh.anchor = TextAnchor.MiddleCenter;
-            textMesh.characterSize = 0.1f;
-            textMesh.offsetZ = -1f;
-            textMesh.color = Color.white;
-            textObject.transform.position = meteorite.transform.position + new Vector3(0, 0, 0);
+
+            currentX += Math.Max(mapSection.cheeseRange.max, mapSection.meteoriteRange.max);
         }
     }
 }
