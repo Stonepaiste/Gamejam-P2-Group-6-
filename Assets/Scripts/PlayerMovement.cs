2026aa3affb1476294ce3cc28591214c26754104
Stonepaiste <mathiasjaque@hotmail.com>
2024-04-25 13:32:36 +0200

    Audio 7

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D body;
-    bool canPlaySound = false;
+    public bool canPlaySound = false;
+    private EventInstance spaceShipLift;
     void Start()
     {
         body= GetComponent<Rigidbody2D>();
+        spaceShipLift = AudioManager.instance.CreatInstance(FmodEvents.instance.spaceShipLift);
     }
 
     
     void FixedUpdate()
     {
         AddForce();
+        UpdateSound();
     }
 
     private void AddForce()
     {
         if (Input.GetMouseButton(0))
         {
-            canPlaySound = Input.GetMouseButtonDown(0);
+            canPlaySound = true;
             body.AddForce(new Vector3(0, 50, 0), ForceMode2D.Force);
             //Debug.Log("Mouse Clicked" + canPlaySound);
 
@@ -31,7 +34,50 @@ public class PlayerMovement : MonoBehaviour
         {
             body.velocity *= 0.25f;
 
-
+        }
+        else
+        {
+            canPlaySound = false;
         }
     }
+
+    
+    
+    private void UpdateSound()
+    
+    
+    {
+        if (canPlaySound == true)
+        {
+            //get playback state
+            PLAYBACK_STATE playbackState;
+            spaceShipLift.getPlaybackState(out playbackState);
+            if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
+           {
+                spaceShipLift.start();
+
+            }
+
+        }
+
+        //otherwise stop the spaceship sound
+        else
+        {
+            spaceShipLift.stop(STOP_MODE.ALLOWFADEOUT);
+
+        }
+
+    }
+    
+   // void waittoStopSound()
+    //{
+      //  spaceShipLift.stop(STOP_MODE.ALLOWFADEOUT);
+      //  Debug.log("Sound Stopped");
+   // }
+
+
+
+
 }
+    
+
