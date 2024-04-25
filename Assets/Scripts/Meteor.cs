2960dcad37cd869db9ec7dceb7c608162fd1117e
PhillzR <rumpelthiin1997@gmail.com>
2024-04-25 21:50:33 +0200

    beam stuff

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -12,7 +12,7 @@ public class Meteor : MonoBehaviour
         playerVFX = FindObjectOfType<PlayerVFX>();
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
