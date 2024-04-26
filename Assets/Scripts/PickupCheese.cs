ffc5365cf5ae566893d91884e722e1cf9f487aff
PhillzR <rumpelthiin1997@gmail.com>
2024-04-26 13:23:21 +0200

    stuff

--- Assets/Scripts/PickupCheese.cs
+++ Assets/Scripts/PickupCheese.cs
@@ -10,7 +10,6 @@ public class PickupCheese : MonoBehaviour
     
     public static PickupCheese Instance = null;
     public int cheeseCounter = 0;
-    [SerializeField] float fallAmount = 0f;
     [SerializeField] float fallSpeed = 0f;
     [SerializeField] float shakeAmount = 0.5f;
     [SerializeField] bool isStinkyCheese = false;
@@ -45,14 +44,15 @@ public class PickupCheese : MonoBehaviour
             {
                 AudioManager.instance.playOneShot(FmodEvents.instance.badCheese, this.transform.position);
             }
-            
+            Debug.Log("Stinky cheese running");
             this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
             this.gameObject.GetComponent<PlayerMovement>().enabled = false;
             fallSpeed = -2f;
             transform.position = new Vector3(transform.position.x, transform.position.y + fallSpeed * Time.deltaTime, transform.position.z);
             Physics.IgnoreLayerCollision(0, 1);
-            if (Input.acceleration.x < -shakeAmount || Input.acceleration.x > shakeAmount || Input.acceleration.y < -shakeAmount || Input.acceleration.y > shakeAmount)
+            if (Input.acceleration.y < -shakeAmount)
             {
+                Debug.Log("Stinky cheese accelerometer");
                 isStinkyCheese = false;
                 this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
                 this.gameObject.GetComponent<PlayerMovement>().enabled = true;
